65d71253ced5ec9ee892781e7c78319e6eb6e30c
RadonRebirth <96480522+RadonRebirth@users.noreply.github.com>
2023-03-24 22:57:30 +0300

    Add files via upload

---
+++ csharpLab11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace csharpLab11
+{
+    class Program
+    {
+        enum weekDays
+        {
+            Понедельник,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
+        delegate void weekDayDelegate();
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            weekDayDelegate weekdelegate = () =>
+            {
+                Console.WriteLine($"{(weekDays)i}");
+                i= (i + 1)%7;
+            };
+
+            for (int j = 0; j < 10; j++)
+            {
+                weekdelegate();
+            }
+        }
+    }
+}
